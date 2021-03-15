46eb073e2cab48071f2936f1288fb414cc3dc6cd
Artur <ryzhiha@gmail.com>
2021-03-15 14:42:05 +0300

    setup search controller

--- Translator/Translate/Views/TranslateViewController.swift
+++ Translator/Translate/Views/TranslateViewController.swift
@@ -8,36 +8,47 @@
 import UIKit
 
 class TranslateViewController: UIViewController {
+    
     //MARK: Subviews
+    
     @IBOutlet var tableView: UITableView!
     
     private lazy var searchController: UISearchController = {
         let sc = UISearchController(searchResultsController: nil)
         sc.searchResultsUpdater = self
         sc.obscuresBackgroundDuringPresentation = false
-        sc.searchBar.placeholder = "Type A Word To Translate"
+        sc.searchBar.placeholder = "Type a word to translate"
         return sc
         
     }()
     
+    //MARK: Instance variables
     
-    
+    //MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
         setupNavigationBar()
-        view.backgroundColor = .white
+        
+        
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    //MARK: Setup Subviews
     private func setupNavigationBar() {
         navigationItem.searchController = searchController
-        navigationController?.navigationBar.backgroundColor = .white
+        navigationItem.hidesSearchBarWhenScrolling = false
+        
+        
     }
     private func setupTableView() {
         tableView.register(TranslationCell.self, forCellReuseIdentifier: TranslationCell.description())
     }
 }
 
-
+//MARK: UITableViewDataSource
 extension TranslateViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         50
@@ -50,10 +61,10 @@ extension TranslateViewController: UITableViewDataSource {
     
     
 }
-
+//MARK: UISearchResultsUpdating
 extension TranslateViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
-        
+       
     }
     
     
