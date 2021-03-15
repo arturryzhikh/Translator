140ab6b9c6db6d1b545cdba24c0073355df178c2
Artur <ryzhiha@gmail.com>
2021-03-15 13:30:52 +0300

    add search bar into navigation bar

---
+++ Translator/Translate/Presenter/TranslatePresenter.swift
@@ -0,0 +1,8 @@
+//
+//  TranslatePresenter.swift
+//  Translator
+//
+//  Created by Artur Ryzhikh on 15.03.2021.
+//
+
+import Foundation
--- Translator/Translate/Views/TranslateViewController.swift
+++ Translator/Translate/Views/TranslateViewController.swift
@@ -8,17 +8,30 @@
 import UIKit
 
 class TranslateViewController: UIViewController {
-
+    //MARK: Subviews
     @IBOutlet var tableView: UITableView!
     
+    private lazy var searchController: UISearchController = {
+        let sc = UISearchController(searchResultsController: nil)
+        sc.searchResultsUpdater = self
+        sc.obscuresBackgroundDuringPresentation = false
+        sc.searchBar.placeholder = "Type A Word To Translate"
+        return sc
+        
+    }()
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
+        setupNavigationBar()
+        view.backgroundColor = .white
     }
-
+    private func setupNavigationBar() {
+        navigationItem.searchController = searchController
+        navigationController?.navigationBar.backgroundColor = .white
+    }
     private func setupTableView() {
         tableView.register(TranslationCell.self, forCellReuseIdentifier: TranslationCell.description())
     }
@@ -33,6 +46,14 @@ extension TranslateViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: TranslationCell.description(), for: indexPath) as! TranslationCell
         return cell
+    }
+    
+    
+}
+
+extension TranslateViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
     }
     
     
